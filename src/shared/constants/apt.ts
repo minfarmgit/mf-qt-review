44e4c6f70f0cbbda558ee5172cbb2136a3352bf8
zidiks <zidiks228@gmail.com>
2025-12-11 10:30:07 +0300

    feat: metrics

--- src/shared/constants/apt.ts
+++ src/shared/constants/apt.ts
@@ -11,5 +11,41 @@ export const APT_DATA: Record<string, AptItem> = {
       yandexMapsUrl: 'https://yandex.by/maps/org/104217020340',
       name: 'Аптека №46',
       code: '46'
-    }
+    },
+    '72': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=10451541020549168043',
+      yandexMapsUrl: 'https://yandex.by/maps/org/215603715988',
+      name: 'Аптека №72',
+      code: '72'
+    },
+    '284': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=8221578538278686684',
+      yandexMapsUrl: 'https://yandex.by/maps/org/202032274042',
+      name: 'Аптека №284',
+      code: '284'
+    },
+    '179': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=13213378986299095279',
+      yandexMapsUrl: 'https://yandex.by/maps/org/36263114292',
+      name: 'Аптека №179',
+      code: '179'
+    },
+    '116': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=16965941201717974211',
+      yandexMapsUrl: 'https://yandex.by/maps/org/1353630857',
+      name: 'Аптека №116',
+      code: '116'
+    },
+    '124': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=8333665823819147265',
+      yandexMapsUrl: 'https://yandex.by/maps/org/168240009084',
+      name: 'Аптека №124',
+      code: '124'
+    },
+    '318': {
+      googleMapsUrl: 'https://maps.google.com/maps?cid=4588264331152748633',
+      yandexMapsUrl: 'https://yandex.by/maps/org/203145677694',
+      name: 'Аптека №318',
+      code: '318'
+    },
 }
